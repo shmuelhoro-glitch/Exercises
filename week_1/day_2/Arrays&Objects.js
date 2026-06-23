234271047db271fe60bf9f50e9b651c9b6d90dbd
shmuelhoro-glitch <shmuelhoro@gmail.com>
2026-06-23 17:24:11 +0300

    array more exercise

--- week_1/day_2/Arrays&Objects.js
+++ week_1/day_2/Arrays&Objects.js
@@ -27,7 +27,41 @@
 // arr.splice(1,1)
 // console.log(arr);
 //9
+// arr.splice(1,0,"x")
+// console.log(arr);
+//10
+// arr.splice(2,1,"Y")
+// console.log(arr);
+//11
+// const numbers = [1,2,3,4,5,6];
+// console.log(numbers.length);
+// console.log(numbers[numbers.length-1]);
 
+//12
+// const arr = [1,"test",true]
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+
+//13
+// const nums = [10,20,30]
+// for (let i= 0;i<nums.length;i++) console.log(nums[i]);
+
+//14
+// const nums = [15,10,5];
+// let current = 0;
+// for (let i = 0; i< nums.length; i++){
+//     current+=nums[i]
+// }
+// console.log(current);
+
+//15
+// const nums = [1,12,8,3]
+// for (let i = 0; i<nums.length;i++){
+//     if (nums[i] > 5){
+//         console.log(nums[i])
+//     };
+// }
 
 
 

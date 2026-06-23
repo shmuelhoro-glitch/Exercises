2a78a98232854f967ad7307af73d686c7ecd665c
shmuelhoro-glitch <shmuelhoro@gmail.com>
2026-06-23 12:00:49 +0300

    Until now

---
+++ week_1/day_1/first.js
@@ -0,0 +1,158 @@
+// const age = 20;
+// age >= 18 ? console.log("adult") : console.log("junior");
+
+// const number = 7;
+// number % 2 == 0 ? console.log("double") : console.log("odd number");
+
+// const isLogging = true;
+// if (isLogging) {
+//   console.log("enter the password");
+// } else {
+//   console.log("welcome");
+// }
+
+// const num = -5;
+// if (num < 0) {
+//   console.log("negative");
+// } else if (num == 0) {
+//   console.log("zero");
+// } else {
+//   console.log("positive");
+// }
+
+// const a = 10;
+
+// const b = 20;
+
+// if (a > b) {
+//   console.log("a bigger");
+// } else if (a < b) {
+//   console.log("b bigger");
+// } else {
+//   console.log("equal");
+// }
+
+// const password = "1234";
+// if (password == "12") {
+//   console.log("Approved access");
+// } else {
+//   console.log("Incorrect password");
+// }
+
+// const x = 15;
+// if (10 <= x && x <= 20) {
+//   console.log("yes");
+// } else {
+//   console.log("no");
+// }
+
+// const grade = 83;
+// if (grade >= 90) {
+//   console.log("excellent");
+// } else if (grade <= 89 && grade >= 75) {
+//   console.log("good");
+// } else if (grade <= 74 && grade >= 60) {
+//   console.log("pass");
+// } else {
+//   console.log("failed");
+// }
+
+// const temp = 28;
+// if (temp > 30) {
+//   console.log("very hot, drink water");
+// } else if (temp <= 30 && temp >= 20) {
+//   console.log("nice outside");
+// } else if (temp < 20) {
+//   console.log("cold, take a jacket");
+// }
+
+// const age = 20;
+// const hasID = true;
+// if (age >= 18 && hasID) {
+//   console.log("Authorized");
+// } else {
+//   console.log("not authorized");
+// }
+
+// const isAdmin = false;
+// const isManager = true;
+// if (isAdmin || isManager) {
+//   console.log("Authorized");
+// } else {
+//   console.log("not authorized");
+// }
+
+// const hasCard = false;
+// if (hasCard) {
+//   console.log("Authorized");
+// } else {
+//   console.log("not authorized");
+// }
+
+// const name = "";
+// if (name) {
+//   console.log(`${name}`);
+// } else {
+//   console.log("guest");
+// }
+
+// const arr = [];
+// if (arr.length) {
+//   console.log(arr);
+// } else {
+//   console.log("empty");
+// }
+
+// const x = "5";
+// const y = 5;
+// console.log(x == y); // בודק בלי לשים לב לטייפ
+// console.log(x === y); // בדיקה קפדנית כוללת טייפ
+
+// const username = "admin";
+// const password = "1234";
+
+// if (username == username && password == password) {
+//   console.log("good");
+// } else {
+//   console.log("bad");
+// }
+
+// const age = 67;
+// if (age < 18) {
+//   console.log("Youth discount");
+// } else if (age > 65) {
+//   console.log("Pensioner discount");
+// } else {
+//   console.log("No discount");
+// }
+
+// const day = "monday";
+// switch (day) {
+//   case "friday":
+//   case "saturday":
+//     console.log("weekend");
+//     break;
+
+//   default:
+//     console.log("work day");
+// }
+
+// const total = 250;
+// if (total > 300) {
+//   console.log("free shipping");
+// } else if (total <= 300 && total >= 150) {
+//   console.log("shipping 20 ₪");
+// } else if (total < 150) {
+//   console.log("sipping 40 ₪");
+// }
+
+// const role = "editor";
+// if (role == "admin") {
+//   console.log("All access");
+// } else if (role == "editor") {
+//   console.log("only edit");
+// } else if (role == "viewer") {
+//   console.log("only view");
+// } else {
+//   console.log("No access");
+// }
---
+++ week_1/day_1/second.js
@@ -0,0 +1,251 @@
+import input from "analiza-sync";
+
+// for (let i = 1; i < 11; i++) {
+//   console.log(i);
+// }
+
+
+
+
+// for (let i = 10; i > 0; i--) {
+//   console.log(i);
+// }
+
+
+
+
+// const numbers = [1, 2, 3, 4, 5];
+// let total = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   total = total + numbers[i];
+// }
+// console.log(total);
+
+
+
+
+// const names = ["dana", "yosi", "michal", "ron"];
+// for (let name of names) {
+//   console.log(name);
+// }
+
+
+
+
+// const numbers = [2, 5, 8, 11, 14, 17, 20];
+// for (let num of numbers) {
+//   if (num % 2 == 0) {
+//     console.log(num);
+//   }
+// }
+
+
+
+
+// const numbers = [3, 99, 12, 45, 78];
+// let max_num = 0;
+// for (let num of numbers) {
+//   if (num > max_num) {
+//     max_num = num;
+//   }
+// }
+// console.log(max_num);
+
+
+
+
+// const word = "javascript";
+// for (let char of word) {
+//   console.log(char);
+// }
+
+
+
+
+// const numbers = [1, 2, 3, 4];
+// for (let num of numbers) {
+//   console.log(num * 2);
+// }
+
+
+
+
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+
+
+
+// let password = "";
+// while (password !== "stop") {
+//   if (password == "1234") {
+//     console.log("welcome");
+//     break;
+//   }
+//   password = input("enter a password ");
+// }
+
+
+
+
+// let num = 0;
+// let userNum = undefined;
+// while (userNum != 0) {
+//   userNum = input("please enter num: ");
+//   num = num + Number(userNum);
+// }
+// console.log(num);
+
+
+
+
+// const secret = 7;
+// let userNum = 0;
+// while (userNum != 7) {
+//   userNum = input("enter a num: ");
+// }
+// console.log("good");
+
+
+
+
+// let x = 1;
+// while (x < 20) {
+//   console.log(x);
+//   x++;
+// }
+
+
+
+
+// let password = "";
+// while (password !== "1234") {
+//   password = input("enter the password: ");
+// }
+// console.log("welcome");
+
+
+
+
+
+// let count = 0;
+// do {
+//   console.log(count);
+// } while (count != 0);
+
+
+
+
+// let choice = "";
+// do {
+//   console.log("menu");
+//   choice = input("enter: ");
+// } while (choice != "exit");
+
+
+
+
+// const numbers = [1, 3, 7, 9, 15, 2];
+// for (let num of numbers) {
+//   if (num > 10) {
+//     break;
+//   }
+//   console.log(num);
+// }
+
+
+
+
+// const numbers = [5, -1, 8, -3, 10];
+// for (let num of numbers) {
+//   if (num < 0) {
+//     continue;
+//   }
+//   console.log(num);
+// }
+
+
+
+
+// const ages = [12, 15, 18, 21, 25];
+// for (let age of ages) {
+//   if (age >= 18) {
+//     console.log(age);
+//     break;
+//   }
+// }
+
+
+
+
+// const arr = [10, 20, 30, 40, 50];
+// for (let num of arr) {
+//   console.log(num);
+// }
+
+
+
+
+// const numbers = [1, 2, 3, 4, 5, 6];
+// let result = 0;
+// for (let num of numbers) {
+//   if (num % 2 == 0) {
+//     result = result + num;
+//   }
+// }
+// console.log(result);
+
+
+
+
+// const target = 25;
+// const numbers = [10, 15, 20, 25, 30];
+// for (let num of numbers) {
+//   if (num == target) {
+//     console.log(num);
+//     break;
+//   }
+// }
+
+
+
+
+// const word = "hello world";
+// for (let char of word) {
+//   if (char != " ") {
+//     console.log(char.toUpperCase());
+//   }
+// }
+
+
+
+
+// const numbers = [2, 4, 6, 8, 10, 12];
+// for (let num of numbers) {
+//   if (num > 8 && num % 2 == 0) {
+//     console.log(num);
+//     break;
+//   }
+// }
+
+
+
+
+// const students = [
+//   { name: "דנה", grade: 55 },
+//   { name: "יוסי", grade: 90 },
+//   { name: "מיכל", grade: 78 },
+//   { name: "רון", grade: 45 },
+// ];
+// for (let st of students) {
+//   if (st["grade"] > 60) {
+//     console.log(st["name"]);
+//   }
+//   if (st["grade"] == 100) {
+//     break;
+//   }
+// }
+

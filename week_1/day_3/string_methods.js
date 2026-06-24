204c2eb9abfdcbfa8d0c8136f7a410a917a19a72
shmuelhoro-glitch <shmuelhoro@gmail.com>
2026-06-24 08:49:53 +0300

    added string methods file

---
+++ week_1/day_3/string_methods.js
@@ -0,0 +1,163 @@
+// לדלג על 5,16,17,18,23,24,25
+
+// 1
+// const str = " hello world "
+// console.log(str);
+// console.log(str.trim());
+
+// 2
+// const checkEmail = "user@gmail.com"
+// console.log(checkEmail.includes("@"));
+
+// 3
+// const word = "javascript"
+// const upperWord = word.toUpperCase()
+// console.log(upperWord);
+
+// 4
+// const word = "HELLO WORLD"
+// const lowerWord = word.toLowerCase()
+// console.log(lowerWord);
+
+
+// 6
+// const word = "JavaScript"
+// const fourFirst = word.slice(0,4)
+// console.log(fourFirst);
+
+// 7
+// const word = "JavaScript"
+// const sixLest = word.slice(-6)
+// console.log(sixLest);
+
+// 8
+// const word = "https://example.com"
+// console.log(word.startsWith("https"));
+
+// 9
+// const word = "mywebsite.org"
+// console.log(word.endsWith(".com"));
+
+// 10
+// const word = "שלום עולם"
+// const changeHello = word.replace("שלום","היי")
+// console.log(changeHello);
+
+// 11
+// const word = "banana"
+// const changeChar = word.replaceAll("a","o")
+// console.log(changeChar);
+
+// 12
+// const words = "one two three"
+// const splitWords = words.split(" ")
+// console.log(splitWords);
+
+// 13
+// const fruits = "apple,banana,grape"
+// const splitAndSlice = fruits.split(",").slice(0,2)
+// console.log(splitAndSlice);
+
+// 14
+// const word = "banana"
+// console.log(word.indexOf("a"));
+
+// 15
+// const word = "banana"
+// console.log(word.lastIndexOf("a"));
+
+// 19
+// const word = "Hello"
+// console.log(word.charAt(0));
+
+// 20
+// const word = "Hello"
+// console.log(word.at(-1));
+
+// 21
+// const word1 = "שלום"
+// const word2 = "עולם"
+// console.log(word1.concat(` ${word2}`));
+
+
+// 22
+// const word = "JavaScript"
+// console.log(word.substring(2,6));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

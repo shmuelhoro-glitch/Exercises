6d790d6765a07239f91788ef4a441c6a83217f6c
shmuelhoro-glitch <shmuelhoro@gmail.com>
2026-06-30 10:28:56 +0300

    completion promises exercises

--- week_2/day_2/promises-fs-practice/app.js
+++ week_2/day_2/promises-fs-practice/app.js
@@ -1,18 +1,19 @@
-import fs from "fs";
+// import fs from "fs";
+
 
 
 // // 1
-function writeFilePromise(filePath, content) {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(filePath, content, "utf8", (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
-}
+// function writeFilePromise(filePath, content) {
+//   return new Promise((resolve, reject) => {
+//     fs.writeFile(filePath, content, "utf8", (err) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve();
+//       }
+//     });
+//   });
+// }
 
 // writeFilePromise("data/message.txt", "Hello world")
 //   .then(() => console.log("file written"))
@@ -20,14 +21,14 @@ function writeFilePromise(filePath, content) {
 
 
 // // 2
-function readFilePromise(filePath) {
-  return new Promise((res, rej) => {
-    fs.readFile(filePath, "utf8", (err, data) => {
-      if (err) rej(err);
-      else res(data);
-    });
-  });
-}
+// function readFilePromise(filePath) {
+//   return new Promise((res, rej) => {
+//     fs.readFile(filePath, "utf8", (err, data) => {
+//       if (err) rej(err);
+//       else res(data);
+//     });
+//   });
+// }
 
 // readFilePromise("data/message.txt")
 //   .then((res) => console.log(res))
@@ -35,14 +36,14 @@ function readFilePromise(filePath) {
 
 
 // // 3
-function readFileAndCount(filePath){
-    return new Promise((res, rej) =>{
-        fs.readFile(filePath,'utf8',(err,data) =>{
-            if (err) rej(err)
-            else res(data.length)
-        })
-    })
-}
+// function readFileAndCount(filePath){
+//     return new Promise((res, rej) =>{
+//         fs.readFile(filePath,'utf8',(err,data) =>{
+//             if (err) rej(err)
+//             else res(data.length)
+//         })
+//     })
+// }
 
 // readFileAndCount('data/message.txt')
 //     .then((res) => console.log(res))
@@ -50,14 +51,14 @@ function readFileAndCount(filePath){
 
 
 // // 4
-function appendFileAsPromise(filePath, content){
-    return new Promise((res,rej) => {
-        fs.appendFile(filePath,content,'utf8',(err) =>{
-            if (err) rej(err)
-            else res()
-        })
-    })
-}
+// function appendFileAsPromise(filePath, content){
+//     return new Promise((res,rej) => {
+//         fs.appendFile(filePath,content,'utf8',(err) =>{
+//             if (err) rej(err)
+//             else res()
+//         })
+//     })
+// }
 
 // appendFileAsPromise('data/step1.txt','shmuel horovitz')
 // .then(() => {
@@ -110,12 +111,23 @@ function appendFileAsPromise(filePath, content){
 // })
 
 
+
+
+// // part 3
+// import fs from "fs/promises"
+
 // // 7
+// fs.readFile("data/final-report.txt",'utf8')
+// .then((data) => console.log(data))
+// .catch(() => console.error)
 
 
-
-
-
+// // 8
+// let file1,file2;
+// fs.readFile('data/report-title.txt','utf8')
+// .then((data) => {file1 = data; return fs.readFile('data/report-body.txt','utf8')})
+// .then((data) => {file2 = data; return fs.writeFile('data/final-report.txt',file1+'\n'+file2)})
+// .finally(() => console.log('done !'))
 
 
 

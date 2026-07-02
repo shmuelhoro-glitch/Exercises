951a94aaf2194a6e4598f8f506c62a8fd7c789cb
shmuelhoro-glitch <shmuelhoro@gmail.com>
2026-07-03 02:24:47 +0300

    add files

---
+++ week_2/projects/movie-manager/app.js
@@ -0,0 +1,9 @@
+
+
+
+function showMenu(){
+    console.log('==============================')
+    console.log('Movie Collection Manager')
+    console.log('==============================')
+    console.log("1. Show all movies \n2. Show by id \n3. Create new movie \n4. Delete movie \n5. Update rate \n6. Search by name \n7. Sort by genre \n8. Show statistics \n9. Exit")
+}

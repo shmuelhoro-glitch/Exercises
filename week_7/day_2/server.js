9b941eeeef8ef22a82d15c592925361471c40813
shmuelhoro-glitch <shmuelhoro@gmail.com>
2026-08-03 11:55:50 +0300

    add form html exercise

---
+++ week_7/day_2/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+
+
+
+
+app.get("/register", (req, res) =>{
+    res.sendFile('register.html', {root: './'})
+})
+
+
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  const { fullName, email, password, age, role, agree } = req.body;
+  if (!fullName || !email){
+    return res.status(400).send("invalid request")
+  };
+  res.status(201).send({ message: "נרשמת!", user: { fullName, email, role } });
+});
+
+app.listen(3000, ()=>{
+    console.log("server listening...")
+})
